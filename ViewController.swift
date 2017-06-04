0c9383a28b9afbd5fce45a14655b7e8b01eb1a39
Daniel-xx <danieltravers78@gmail.com>
2017-06-04 16:18:40 +0100

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Exercise1
+//
+//  Created by Daniel Travers on 04/06/2017.
+//  Copyright © 2017 Daniel Travers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Exercise 1")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
